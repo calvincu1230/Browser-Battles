dbc8fda2b16b29d5fb7c8a8dfe777c166875445c
ccurnuck <calvin.curnuck@gmail.com>
2020-03-04 14:31:46 -0500

    Added Initial Game Menu

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,13 +1,19 @@
 export default class Menu {
   constructor(gameWidth, gameHeight) {
-    this.gamewidth = gameWidth; 
-    this.gameheight = gameHeight;
+    this.gameWidth = gameWidth; 
+    this.gameHeight = gameHeight;
   }
 
   draw(ctx) {
     ctx.rect(0, 0, this.gameWidth, this.gameHeight);
-    ctx.fillStyle = "red";
+    ctx.fillStyle = "rgba(0, 0, 0, 0.75)";
     // put inner text with a listener for any button click to go to character screen
     ctx.fill();
+
+    ctx.font = "40px Arial";
+    ctx.fillStyle = "white";
+    ctx.textAlign = "center";
+    ctx.fillText("Press Any Key to Battle", this.gameWidth / 2, this.gameHeight / 2);
+
   }
 }
