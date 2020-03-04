cbf7bbbf710f85149646a32e0707ea5ba4a3960b
ccurnuck <calvin.curnuck@gmail.com>
2020-03-04 14:51:54 -0500

    Updated event listener to listen only once

--- src/js/menu_input.js
+++ src/js/menu_input.js
@@ -3,11 +3,15 @@ import Player from "./player";
 import Computer from "./computer";
 
 export const menuInput = (gameHeight, gameWidth, ctx) => {
+  let count = 0;
   document.addEventListener("keydown", (e) => {
-    const player = new Player(100, 20, "Chrome", gameHeight, gameWidth);
-    const computer = new Computer(100, 20, "Internet_Explorer", gameHeight, gameWidth);
-    // refactor eventually to add player based on key in puts on later screen
-    const game = new Game(player, computer);
-    game.start(ctx); // for now starts game with default browsers
+    if (count < 1) { // this is janky logic, maybe add removeEventListener later
+      count++;
+      const player = new Player(100, 20, "Chrome", gameHeight, gameWidth);
+      const computer = new Computer(100, 20, "Internet_Explorer", gameHeight, gameWidth);
+      // refactor eventually to add player based on key in puts on later screen
+      const game = new Game(player, computer);
+      game.start(ctx); // for now starts game with default browsers
+    } else return;
   });
 }
