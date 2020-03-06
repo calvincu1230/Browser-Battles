3b540d7bc99185a5c5aa22a3e61c9935ddcff0c9
ccurnuck <calvin.curnuck@gmail.com>
2020-03-06 12:24:22 -0500

    Initial BattleOptions Logic

--- src/js/battle_options.js
+++ src/js/battle_options.js
@@ -1,7 +1,41 @@
 export default class BattleOptions {
   constructor(player) {
     this.player = player;
-    this.selected = attackBtn;
+    this.gameHeight = player.gameHeight;
+    this.gameWidth = player.gameWidth;
+    this.height = 125;
+    this.width = 400;
+    this.selected = 0;
+    this.optionsText = ["Attack", "Heal", "Quit"]
+    this.options = [this.attack, this.heal, this.quit];
+    this.position = {
+      x: this.gameWidth - this.width - 50,
+      y: this.gameHeight - this.height - 30
+    }
+  }
+
+  // update(dt) {
+  //   if (!dt) {
+
+  //   }
+
+
+  // }
+
+  draw(ctx){
+    ctx.rect(this.position.x, this.position.y, this.width, this.height);
+    ctx.fillStyle = "white";
+    ctx.fill();
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = "black";
+    ctx.stroke();
+    const options = this.optionsText.join("    ");
+    ctx.font = "30px Arial";
+    ctx.fillStyle = "black";
+    // ctx.textAlign = "center";
+    ctx.fillText(options, this.position.x + this.width / 6, this.position.y + this.height / 1.75);
+    ctx.beginPath();
+    ctx.moveTo(this.position.x + this.width / 6, );
   }
 
   attack(currentPlayer, opponent) {
