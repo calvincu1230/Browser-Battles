69c0bed72530200547f1f06c5948f8339eb349b4
ccurnuck <calvin.curnuck@gmail.com>
2020-03-06 12:23:19 -0500

    General Ideas on game flow

---
+++ pseudo.js
@@ -0,0 +1,12 @@
+// Game Will have attr attacking that will be true when
+// computer or player are attacking
+// When attack is started, set true
+// if game.currentPlayer.attacking, call update and draw
+// maybe will also continue until attack animation is over and
+// health is decremented to correct value
+// if attacking, then render statusText over battle options
+// set false when attack is over
+// player will have while loop that will update
+
+// if selected option is < 0; selection === options.length - 1
+// if selected option is > options.length - 1; selection === 0
