1fb62ceae82c8a809c8bcddcfbfa1b0d1e7da2c3
ccurnuck <calvin.curnuck@gmail.com>
2020-04-01 14:45:11 -0400

    Added multi line status

--- src/js/status_text.js
+++ src/js/status_text.js
@@ -3,7 +3,10 @@ export default class StatusText {
     this.gameHeight = gameHeight;
     this.gameWidth = gameWidth;
     this.finalStatus = status;
-    this.currentStatus = "";
+    this.currentStatus1 = "";
+    this.currentStatus2 = "";
+    this.statusLine1 = "";
+    this.statusLine2 = "";
     this.height = 125;
     this.width = 500;
     this.count = 0;
@@ -14,10 +17,45 @@ export default class StatusText {
     }
     this.draw = this.draw.bind(this);
     this.update = this.update.bind(this);
-    // this.currentStatus2
-    // this.call the method that will split it up
+    this.checkLineLength(status);
   } 
   // add a method that will split text to two lines if over 
+  checkLineLength(status) {
+    const statusArr = status.split(" ");
+    if (statusArr.length > 6) {
+      this.statusLine1 = statusArr.slice(0, 6).join(" ");
+      this.statusLine2 = statusArr.slice(6).join(" ");
+    } else {
+      this.statusLine1 = status;
+    }
+  }
+
+  drawWinnerText(ctx) {
+    ctx.font = '20px "Press Start 2P"';
+    ctx.fillStyle = "black";
+    const width = this.position.x + 15;
+    ctx.fillText(this.currentStatus1, width + 50, this.position.y + this.height / 1.75);
+  }
+
+  drawMultiLineStatus(ctx) {
+    const height = this.position.y + this.height / 3 + 15; // current line height
+    // if (this.count.length < this.finalStatus.length) {
+    this.drawSingleLine(ctx, height);
+
+    if (this.count >= this.statusLine1.length) {
+      this.drawSingleLine(ctx, height + 25, this.currentStatus2); // changes line height 
+    }
+  }
+
+  drawSingleLine(ctx, height, status) {
+    if (height === undefined) height = this.position.y + this.height / 1.75;
+    if (status === undefined) status = this.currentStatus1;
+    
+    ctx.font = '12px "Press Start 2P"';
+    ctx.fillStyle = "black";
+    const width = this.position.x + 15;
+    ctx.fillText(status, width, height);
+  }
 
   draw(ctx) {
     ctx.rect(this.position.x, this.position.y, this.width, this.height);
@@ -26,20 +64,32 @@ export default class StatusText {
     ctx.lineWidth = 5;
     ctx.strokeStyle = "black";
     ctx.stroke();
-    ctx.font = '12px "Press Start 2P"';
-    ctx.fillStyle = "black";
-    const width = this.position.x + 20;
-    ctx.fillText(this.currentStatus, width, this.position.y + this.height / 1.75);
+
+    if (this.finalStatus.includes('wins')) {
+      this.drawWinnerText(ctx);
+    } else if (this.statusLine1 && this.statusLine2) {
+      this.drawMultiLineStatus(ctx);
+    } else {
+      this.drawSingleLine(ctx);
+    }
   }
 
   update() {
-    if (this.finalStatus.includes('win')) {
-      this.currentStatus = this.finalStatus;
+    if (this.finalStatus.includes('wins')) {
+      this.currentStatus1 = this.finalStatus;
       return;
     }
     if (this.count < this.length) {
-      this.currentStatus += this.finalStatus[this.count];
-      this.count++;
+      if (this.count < this.statusLine1.length) {
+        this.currentStatus1 += this.finalStatus[this.count];
+        this.count++;
+      } else if (this.count === this.statusLine1.length) {
+        this.count++;
+        return;
+      } else {
+        this.currentStatus2 += this.finalStatus[this.count];
+        this.count++;
+      }
     }
   }
 }
